369c94e399697257c37a867fc3a56329e4249425
Bird <example@example.com>
2024-11-17 21:27:06 +0000

    use lib function, remove experiment clutter

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ import math
 
 from cryptography.fernet import Fernet
 
+import lib
 from lib.constants import default_port
 
 
@@ -16,27 +17,9 @@ def main():
     print(f'Example generated key: {generated_key}')
     print(f'Length of example key: {len(generated_key)}')
 
-    required_key_length_bits = 256 # 32 * 8
-    required_key_length_bytes = required_key_length_bits / 8
-    required_key_length_characters = int(math.ceil(required_key_length_bits / 6.0))
-    print(f'required_key_length_characters={required_key_length_characters}')
-
-    key = "JuliaJuliaJuliaJuliaJuliaJuliaJulia"
-    #key = "1234567890123456789012345678901234567890"
-    print(f'Encryption key is: {key}')
-    base64_key = base64.b64encode(key.encode('utf-8'))
+    password = "JuliaJuliaJuliaJuliaJuliaJuliaJulia"
+    base64_key = lib.get_fernet_key_from_password(password)
     print(f'Base64 encoded key is: {base64_key}')
-    print(f'Length of base64 encoded key: {len(base64_key)}')
-    if len(base64_key) > required_key_length_characters:
-        base64_key = base64_key[:required_key_length_characters]
-        print(f'Truncating key to length {required_key_length_characters}: {base64_key}')
-        print(f'Length of base64 encoded key is now {len(base64_key)}')
-        base64_key = base64_key + b'='
-        print(f'Adding padding: {base64_key}')
-        print(f'Length of base64 encoded key is now {len(base64_key)}')
-    elif len(base64_key) < required_key_length_characters:
-        print(f'error: key is too short')
-        raise RuntimeError(f'key too short')
 
     fernet_instance = Fernet(base64_key)
     token = fernet_instance.encrypt(b"some example content to encrypt")
