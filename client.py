4bbe24585f756cde14548b761d79eb97e07f90c5
edward-bestx <example@example.com>
2024-11-19 21:31:14 +0000

    use 256 shasum to generate Fernet key

--- client.py
+++ client.py
@@ -3,6 +3,7 @@
 import socket
 import ssl
 import base64
+import hashlib
 import math
 
 from cryptography.fernet import Fernet
@@ -18,7 +19,12 @@ def main():
     print(f'Length of example key: {len(generated_key)}')
 
     password = "JuliaJuliaJuliaJuliaJuliaJuliaJulia"
-    base64_key = lib.get_fernet_key_from_password(password)
+    hashed_password = hashlib.sha256(password.encode('utf-8'))
+    hash_digest = hashed_password.digest()
+    print(f'{hashed_password.hexdigest()}')
+    #base64_key = lib.get_fernet_key_from_password(password)
+    #print(f'Base64 encoded key is: {base64_key}')
+    base64_key = lib.get_fernet_key_from_password_2(password)
     print(f'Base64 encoded key is: {base64_key}')
 
     fernet_instance = Fernet(base64_key)
@@ -28,6 +34,11 @@ def main():
 
     hostname = 'localhost'
     context = ssl._create_unverified_context()
+
+    path = 'example-file.txt'
+    file_hash = calculate_file_hash(path)
+
+    return
 
     with socket.create_connection((hostname, default_port)) as client_socket:
         with context.wrap_socket(client_socket, server_hostname=hostname) as ssocket:
