c80291d07f30041af77f5c6a25d97b75cee8fdf4
edward-bestx <example@example.com>
2024-11-20 21:48:54 +0000

    added controller.py

---
+++ controller.py
@@ -0,0 +1,11 @@
+
+
+
+def main():
+
+    control_port = DEFAULT_CONTROL_PORT
+    with socket.create_connection(('localhost', control_port)) as control_socket:
+
+
+if __name__ == '__main__':
+    main()
