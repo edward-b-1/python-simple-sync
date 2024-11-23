1faf2b4ef7f86e95173d8c64a9f5b96e60bc22f3
Bird <example@example.com>
2024-11-23 12:42:02 +0000

    fixed fast_scandir functions

--- lib_py_simple_sync/directory_list/fast_scandir.py
+++ lib_py_simple_sync/directory_list/fast_scandir.py
@@ -30,28 +30,72 @@ def fast_scandir_2(target_dir: str) -> list[str]:
 
 
 def _fast_scandir_3_impl_get_paths_recursive(target: str) -> list[str]:
-    #print(f'_fast_scandir_3_impl_get_paths_recursive({target})')
     items:list[str] = []
     if os.path.isfile(target):
-        #print(f'{target} is file')
         items.append(target)
     elif os.path.isdir(target):
-        #print(f'{target} is directory')
         items.append(target)
-        # items.extend(
-        #     #list(
+        # joined_items = list(
         #         map(
-        #             _fast_scandir_3_impl_get_paths_recursive,
+        #             lambda item: os.path.join(target, item),
         #             os.listdir(target),
         #         )
-        #     #)
+        # )
+        # print('joined_items')
+        # print(joined_items)
+        # items.extend(
+        #     map(
+        #         _fast_scandir_3_impl_get_paths_recursive,
+        #         joined_items
+        #     )
         # )
         for item in os.listdir(target):
-            item = os.path.join(target, item)
-            #print(f'os.listdir: {item}')
-            more_items = _fast_scandir_3_impl_get_paths_recursive(item)
-            items.extend(more_items)
+           item = os.path.join(target, item)
+           more_items = _fast_scandir_3_impl_get_paths_recursive(item)
+           items.extend(more_items)
     return items
+
+
+def _fast_scandir_3_impl_convert_paths_to_path_tuples(paths:list[str]) -> list[tuple[str, str]]:
+    def path_to_tuple(path:str) -> tuple[str, str]:
+        if os.path.isfile(path):
+            return ('f', path)
+        elif os.path.isdir(path):
+            return ('d', path)
+
+    return (
+        list(
+            map(
+                path_to_tuple,
+                paths,
+            )
+        )
+    )
+
+
+def _fast_scandir_3_impl_convert_path_tuples_to_fully_qualified_path_tuples(
+    path_tuples:list[tuple[str, str]],
+) -> list[tuple[str, str]]:
+    return (
+        list(
+            map(
+                lambda path_tuple: (path_tuple[0], os.path.abspath(path_tuple[1])),
+                path_tuples,
+            )
+        )
+    )
+
+
+def _fast_scandir_3_impl_sort_path_tuples(
+    path_tuples:list[tuple[str, str]],
+) -> list[tuple[str, str]]:
+    return (
+        sorted(
+            path_tuples,
+            key=lambda path_tuple: path_tuple[1],
+        )
+    )
+
 
 def _fast_scandir_3_impl(target:tuple[str, str]) -> list[tuple[str, str]]:
     items:list[tuple[str, str]] = []
@@ -81,21 +125,32 @@ def _fast_scandir_3_fully_qualified_sorted_impl(target:tuple[str, str]) -> list[
 def fast_scandir_3(target:str) -> list[tuple[str, str]]:
 
     # get all paths
-    _fast_scandir_3_impl_get_paths_recursive(target)
+    paths = _fast_scandir_3_impl_get_paths_recursive(target)
 
     # convert paths to tuple depending on type
+    path_tuples = _fast_scandir_3_impl_convert_paths_to_path_tuples(paths)
 
     # convert paths to fully qualified paths
+    fully_qualified_path_tuples = (
+        _fast_scandir_3_impl_convert_path_tuples_to_fully_qualified_path_tuples(
+            path_tuples,
+        )
+    )
 
     # sort by fully qualified path
+    sorted_fully_qualified_path_tuples = (
+        _fast_scandir_3_impl_sort_path_tuples(fully_qualified_path_tuples)
+    )
 
-    items:list[tuple[str, str]] = []
-    if os.path.isfile(target):
-        item = ('f', target)
-        items.append(item)
-        items.extend(_fast_scandir_3_impl(item))
-    elif os.path.isdir(target):
-        item = ('d', target)
-        items.append(item)
-        items.extend(_fast_scandir_3_impl(item))
-    return items
+    return sorted_fully_qualified_path_tuples
+
+    # items:list[tuple[str, str]] = []
+    # if os.path.isfile(target):
+    #     item = ('f', target)
+    #     items.append(item)
+    #     items.extend(_fast_scandir_3_impl(item))
+    # elif os.path.isdir(target):
+    #     item = ('d', target)
+    #     items.append(item)
+    #     items.extend(_fast_scandir_3_impl(item))
+    # return items
