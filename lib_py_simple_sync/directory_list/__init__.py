7974eeb48cc5f0c6fab178ce48fdbe15245208ad
Bird <example@example.com>
2024-11-23 08:05:34 +0000

    moved duplicate copy of fast_scandir_2

--- lib_py_simple_sync/directory_list/__init__.py
+++ lib_py_simple_sync/directory_list/__init__.py
@@ -33,13 +33,4 @@ def fast_scandir(dirname: str) -> list[str]:
     return subfolders
 
 
-def fast_scandir_2(dirname: str) -> list[str]:
-    files = []
-    for f in os.scandir(dirname):
-        if f.is_dir():
-            files.extend(fast_scandir_2(f.path))
-        if f.is_file():
-            files.append(f.path)
-    files.sort()
-    return files
 
