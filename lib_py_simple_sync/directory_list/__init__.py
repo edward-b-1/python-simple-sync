f42a6cf67aad7c109399ae5eb42b7604301a7273
Bird <example@example.com>
2024-11-23 15:10:14 +0000

    removed obsolete function

--- lib_py_simple_sync/directory_list/__init__.py
+++ lib_py_simple_sync/directory_list/__init__.py
@@ -1,29 +1,2 @@
-# ./property-price-analysis-analysis/Rightmove-Analysis/combine_exported_data.py
-# ./Land-Registry-Data-Ingestion/Land-Registry-Download/minio_upload_pp_monthly_update_files_from_devbox2.py
-# ./polygon-fx/filesystem_helper_functions.py
-
-
-import os
-
-
-def get_directory_list(target: str) -> list[(str, str)]:
-
-    if os.path.isfile(target):
-        item_file = (target, 'f')
-        items = [item_file]
-        return items
-
-    elif os.path.isdir(target):
-        item_dir = (target, 'd')
-        items = [item_dir]
-        # TODO: map
-        for item in os.listdir(target):
-            full_path = os.path.join(target, item)
-            more_items = get_directory_list(full_path)
-            items.extend(more_items)
-        return items
-
-    return []
-
 
 
